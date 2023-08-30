2ec2306d84cb17d6fc17a9fc42b1d75b0734c5fc
Kevin Ortega <kevin.ortega2011@gmail.com>
2023-08-30 17:37:26 -0400

    solution for the 06 task

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,43 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    // if(year % 100 === 0 || year % 4 === 0) {
+    //     if(year % 400 === 0) return true; 
+    //     return false;
+    // }
+
+    // if(year % 4 === 0 || year % 4 === 400) {
+    //     return true;
+    // }
+    // if(year % 100 == 0) {
+    //     if(year % 400 == 0) {
+    //         return true;
+    //     }
+    // } else if(year % 4 == 0 && year % 100 != 0) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    // this is a valid and correct way
+    // if (year % 4 === 0) {
+    //     if (year % 100 === 0) {
+    //         if(year % 400 === 0) return true;
+    //         else return false;
+    //     } else {
+    //         return true;
+    //     }
+    // } else {
+    //     return false;
+    // }
+
+    //this is the solution provided and the most compact and easiest way
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+    
 };
+
+//Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years 
+//(such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). 
+//(Yes, it's all pretty confusing)
 
 // Do not edit below this line
 module.exports = leapYears;
