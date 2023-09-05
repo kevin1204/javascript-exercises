90adb4e8237b2f07174c350311070605fbfafd1a
Kevin Ortega <kevin.ortega2011@gmail.com>
2023-09-05 01:51:33 -0400

    adding solution to the 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const arry1 = string.split('');
+    const arry2 = [];
+    for(let i = arry1.length - 1; i>=0; i--) {
+        arry2.push(arry1[i]);
+    }
+    
+    //ser a variabler with the punctation marks!
+    const regex = /[ \!\@\#\$\%\^\&\*\(\)\_\+\-\=\[\]\{\}\\\|\'\"\;\:\,\.\<\>\/\?]/g;
+    
+    let string2 = arry2.join('');
 
+    string = string.replace(regex, '').toLowerCase();
+    string2 = string2.replace(regex, '').toLowerCase();
+    
+    return string === string2 ? true : false;
 };
 
+// console.log(palindromes('hola como estas?/'));
 // Do not edit below this line
 module.exports = palindromes;
