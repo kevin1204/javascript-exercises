4ad0bef8c283dfe6498e5f5d7becd50a9ab7c9e9
Kevin Ortega <kevin.ortega2011@gmail.com>
2023-09-07 13:07:18 -0400

    solution to the exercise 11 added

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // return books.map(book => book.title);
+
+    //the callback function is basically like saying this
+    // function(book) {
+    //     return book.title;
+    // }
+
+    //under this principle we can then also code like this: 
+
+    let titles = books.map(function(book) {
+        return book.title;
+    });
+
+    return titles;
 
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
